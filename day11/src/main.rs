c3d1d552c2a52872dd2f5a3f282d3456f3c517a3
Michael Auracher <michael.auracher@gmail.com>
2019-08-04 17:49:45 +0200

    Add some comments for day11.

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -9,6 +9,13 @@ const MAX_GRID: usize = 300;
 
 fn get_best_for_square(field: &[Vec<i64>], square_size: usize) -> (i64, usize, usize, usize) {
     let mut results = Vec::new();
+    //For the part of the board that the square fits on calculate the values
+    //by taking our precomputed values from before:
+    // | a | b | c |
+    // | d | e | f |
+    // | g | h | i |
+    // -> The value for position e is calculated by taking val(e) = i + a - c - g;
+    //because of the additional row and column 0 we don't have to check indicies here.
     for x in 1..=MAX_GRID - square_size {
         for y in 1..=MAX_GRID - square_size {
             let value = field[x + square_size - 1][y + square_size - 1] 
@@ -25,26 +32,26 @@ fn get_best_for_square(field: &[Vec<i64>], square_size: usize) -> (i64, usize, u
 
 fn main() {
     let serial_number = 9810;
-    let mut power_levels = vec![vec![0; MAX_GRID + 1]; MAX_GRID + 1];
+    let mut precomputed_field = vec![vec![0; MAX_GRID + 1]; MAX_GRID + 1];
     //Precalculate the power of all sums from the top left corner to the current (x, y)
     //For convenience we also increase the board size by the 0 row and 0 column.
     for x in 0..=MAX_GRID {
         for y in 0..=MAX_GRID {
             if x == 0 || y == 0 { 
-                power_levels[x][y] = 0;
+                precomputed_field[x][y] = 0;
             } else {
                 let power_level = calculate_power((x, y), serial_number);
-                power_levels[x][y] = power_level 
-                                    + power_levels[x][y - 1] 
-                                    + power_levels[x - 1][y] 
-                                    - power_levels[x - 1][y - 1];
+                precomputed_field[x][y] = power_level 
+                                    + precomputed_field[x][y - 1] 
+                                    + precomputed_field[x - 1][y] 
+                                    - precomputed_field[x - 1][y - 1];
             }
         }
     }
 
-    let max1 = get_best_for_square(&power_levels, 3);
+    let max1 = get_best_for_square(&precomputed_field, 3);
     println!("Result part1 is: ({}, {}) score: {}", max1.1, max1.2, max1.0);
 
-    let max_overall = (2..MAX_GRID).map(|square_size| get_best_for_square(&power_levels, square_size)).max().unwrap();
+    let max_overall = (2..MAX_GRID).map(|square_size| get_best_for_square(&precomputed_field, square_size)).max().unwrap();
     println!("Result part2 is: ({}, {}, {}) score: {}", max_overall.1, max_overall.2, max_overall.3, max_overall.0);
 }
